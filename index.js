0a4942be56b0998f05e2e3dc8a6ae8a598fc4893
nebylica <v.nebylica@gmail.com>
2021-03-12 15:22:33 +0200

    chage3

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 let word = document.getElementById('word')
+let letter = document.getElementById('letter')
+let guessBtn = document.getElementById('guessBtn')
+let lives = document.getElementById('lives')
+
+
 
 
 let cities = [
@@ -9,8 +14,10 @@ let cities = [
     "singapore",
     "dubai"
 ];
+let livesCounter = 10
 
 let city = cities[Math.floor(Math.random() * cities.length)]
+console.log(city)
 
 let answerArray = [];
 
@@ -18,4 +25,22 @@ let answerArray = [];
 for (let i = 0; i < city.length; i++) {
     answerArray[i] = "_";
     word.innerText = answerArray.join(' ')
+}
+
+guessBtn.addEventListener('click', checkLetter)
+
+function checkLetter() {
+
+    for (let i = 0; i < city.length; i++) {
+        if (city[i] === letter.value) {
+            answerArray[i] = letter.value;
+            word.innerText = answerArray.join(' ')
+        }
+    }
+
+    if(!city.includes(letter.value)) {
+        livesCounter -=1
+        lives.innerText =`Lives Left : ${livesCounter}`
+    }
+
 }
