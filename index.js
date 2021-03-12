c13e43d8745577582439e42cef2610ad6c9e3abc
nebylica <v.nebylica@gmail.com>
2021-03-12 16:03:40 +0200

    change5

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ let letter = document.getElementById('letter')
 let guessBtn = document.getElementById('guessBtn')
 let lives = document.getElementById('lives')
 let wonLose = document.getElementById('wonLose')
+let one = document.getElementById('one')
+let two = document.getElementById('two')
+let three = document.getElementById('three')
+let four = document.getElementById('four')
+let five = document.getElementById('five')
+let six = document.getElementById('six')
+let seven = document.getElementById('seven')
+let eight = document.getElementById('eight')
+let nine = document.getElementById('nine')
+let ten = document.getElementById('ten')
 
 
 
@@ -44,6 +54,7 @@ function checkLetter() {
     }
 
     gameChecker()
+    hangman()
 }
 
 function gameChecker() {
@@ -57,4 +68,17 @@ function gameChecker() {
         wonLose.innerText = 'Win'
         wonLose.style.color = 'green'
     }
+}
+
+function hangman() {
+    livesCounter === 9 ? one.style.display = 'block' : null
+    livesCounter === 8 ? two.style.display = 'block' : null
+    livesCounter === 7 ? three.style.display = 'block' : null
+    livesCounter === 6 ? four.style.display = 'block' : null
+    livesCounter === 5 ? five.style.display = 'block' : null
+    livesCounter === 4 ? six.style.display = 'block' : null
+    livesCounter === 3 ? seven.style.display = 'block' : null
+    livesCounter === 2 ? eight.style.display = 'block' : null
+    livesCounter === 1 ? nine.style.display = 'block' : null
+    livesCounter === 0 ? ten.style.display = 'block' : null
 }
